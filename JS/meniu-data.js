07509e23b0bd823728df02a4e96aee22d2b80cbb
LaNotteGalati <lanottegalati@gmail.com>
2026-03-09 16:25:43 +0200

    Add files via upload

---
+++ JS/meniu-data.js
@@ -0,0 +1,175 @@
+const menu = {
+
+Aperitive: [
+{
+name: "Platou brânzeturi 350g",
+price: "46 LEI",
+image: "../Images/Meniu/Aperitive/platou-branzeturi.jpg",
+desc: "Brie, gorgonzola, cheddar, struguri, nuci"
+},
+{
+name: "Bruschete mix 200g",
+price: "22 LEI",
+image: "../Images/Meniu/Aperitive/bruschete.jpg",
+desc: "Rosii/busuioc, ciuperci/usturoi, pastă de măsline"
+},
+{
+name: "Prosciutto & mozzarella 200g",
+price: "38 LEI",
+image: "../Images/Meniu/Aperitive/Prosciutto.jpg",
+desc: "Prosciutto crud, mozzarella, roșii cherry, rucola"
+},
+{
+name: "Aripioare picante 350g",
+price: "32 LEI",
+image: "../Images/Meniu/Aperitive/Aripioare.jpg",
+desc: "Aripioare glazurate, sos Buffalo"
+},
+{
+name: "Calamari pane 200g",
+price: "42 LEI",
+image: "../Images/Meniu/Aperitive/calamari.webp",
+desc: "inele de calamari prăjiți, sos aioli"
+}
+],
+
+Paste: [
+{
+name: "Paste cu sos roșu 350g",
+price: "28 LEI",
+image: "../Images/Meniu/Paste/Paste cu sos de rosii.jpg",
+desc: "Roșii, busuioc, ulei de măsline, parmezan, paste"
+},
+
+{
+name: "Paste cu ricotta și sos alb 400g",
+price: "30 LEI",
+image: "../Images/Meniu/Paste/Paste cu ricotta si sos alb.jpg",
+desc: "Smântână, oregano, usturoi, parmezan"
+},
+
+{
+name: "Paste carbonara 350g",
+price: "36 LEI",
+image: "../Images/Meniu/Paste/Paste carbonara.jpg",
+desc: "Guanciale, ou, pecorino, ardei iute"
+},
+
+{
+name: "Paste cu fructe de mare 390g",
+price: "46 LEI",
+image: "../Images/Meniu/Paste/Paste cu fructe de mare.png",
+desc: "Fructe de mare , usturoi, vin alb, pătrunjel"
+}
+],
+
+Pizza: [
+{
+name: "Margherita 620g",
+price: "40 LEI",
+image: "../Images/Meniu/Pizza/Pizza margherita.jpg",
+desc: "sos de roșii, mozzarella, busuioc, ulei de măsline"
+},
+
+{
+name: "Capricciosa 630g",
+price: "40 LEI",
+image: "../Images/Meniu/Pizza/Capriciosa.jpg",
+desc: "sos, mozzarella, salam, șuncă, ciuperci, măsline"
+},
+
+{
+name: "Pizza La Notte 700g",
+price: "43 LEI",
+image: "../Images/Meniu/Pizza/La Notte.png",
+desc: "sos, mozzarella, prosciutto, busuioc, roșii cherry"
+},
+
+{
+name: "Quattro Formaggi 630g",
+price: "40 LEI",
+image: "../Images/Meniu/Pizza/Quattro Formaggi.jpg",
+desc: "sos, mozzarella, parmezan, gorgonzola, pecorino"
+},
+
+{
+name: "Diavola 575g",
+price: "38 LEI",
+image: "../Images/Meniu/Pizza/Diavola.jpg",
+desc: "Sos, mozzarella, salam, chili"
+},
+],
+
+Lasagna: [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+],
+
+Salate: [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+],
+
+Deserturi: [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+],
+
+"Meniu copii": [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+],
+
+"Băuturi non-alcoolice": [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+],
+
+"Băuturi alcoolice": [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+],
+
+"Cafele Calde": [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+],
+
+"Cafele reci": [
+{
+name: "Tiramisu",
+price: "$9",
+image: "../images/tiramisu.jpg",
+desc: "Mascarpone cream and espresso"
+}
+]
+
+};
